261b79a03d9c8a0af0aaa74ffd6862bba2281a1d
vimiomori <vivian.muchen@gmail.com>
2020-03-20 17:56:14 +0900

    style popup

--- popup.js
+++ popup.js
@@ -1,12 +1,8 @@
-// TODO: https://developer.chrome.com/extensions/messaging
-// use long term connection
+const activate = document.querySelector('input');
 
-let activate = document.querySelector('input');
 activate.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {activate: activate.checked}, function(response) {
-      // activate.classList.add("hidden")
-      // deactivate.classList.remove("hidden")
     });
   });
 };
