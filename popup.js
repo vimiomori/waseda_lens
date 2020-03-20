b3fcfb02fffb2e03365a3950c5c4ef04df485a58
vimiomori <vivian.muchen@gmail.com>
2020-03-20 16:04:21 +0900

    add slider

--- popup.js
+++ popup.js
@@ -1,24 +1,12 @@
 // TODO: https://developer.chrome.com/extensions/messaging
 // use long term connection
 
-let activate = document.getElementById('activate');
-let deactivate = document.getElementById('deactivate');
+let activate = document.querySelector('input');
 activate.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {activate: true}, function(response) {
-      activate.classList.add("hidden")
-      deactivate.classList.remove("hidden")
-    });
-  });
-};
-
-deactivate.onclick = function(element) {
-  console.log(element)
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {activate: false}, function(response) {
-      activate.classList.remove("hidden")
-      deactivate.classList.add("hidden")
-      console.log(response.deactivated);
+    chrome.tabs.sendMessage(tabs[0].id, {activate: activate.checked}, function(response) {
+      // activate.classList.add("hidden")
+      // deactivate.classList.remove("hidden")
     });
   });
 };
